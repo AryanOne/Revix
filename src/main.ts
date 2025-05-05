c1f5c6f3bd38687e6da2cac61e0fd9ac1f64413f
Aryan <xoneder1010@gmail.com>
2025-05-05 20:05:39 +0530

    created init command

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
-let a:number=10;
-console.log(a);
+import {argv} from "node:process";
+import fs from "node:fs";
+
+const args=argv;
+
+if(args[2]==="init"){
+    try{
+        fs.mkdirSync(".lit",{recursive:true});
+        fs.mkdirSync(".lit/objects",{recursive:true});
+        fs.mkdirSync(".lit/refs",{recursive:true});
+        fs.writeFileSync(".lit/HEAD","ref: refs/heads/main\n");
+        console.log("lit intialized")
+    }
+    catch(e){
+        console.log("error creating .lit ",e);
+    }
+}else{
+    console.log("wrong input commmad");
+}
