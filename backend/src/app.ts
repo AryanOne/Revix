e7781a22929b64054850503e4f3267119dfb6a52
Aryan <xoneder1010@gmail.com>
2025-07-08 19:42:18 +0530

    basic auth routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "../generated/prisma";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const prisma=new PrismaClient();
@@ -20,20 +21,73 @@ app.post("/signup",async (req: Request, res: Response) => {
     
         const user=await prisma.user.create({
             data:{
-                username:username,
+                username,
                 email,
                 password:hashPassword
             }
         })
-        res.json({
-            user
-        })
+
+        const token=jwt.sign({userId:user.id},process.env.JWT_SECRET!,{ expiresIn: "7d" })
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "lax",                          
+            secure: process.env.NODE_ENV === "production", 
+            maxAge: 1000 * 60 * 60 * 24 * 7           
+        });
+
+        res.send("Signup successfull");
+
     }catch(err){
         console.log(err);
         res.send("Something went wrong..")
     }
     
 });
+
+app.post("/login",async(req:Request,res:Response)=>{
+    
+    const {email,password}=req.body;
+
+    if(!email || !password){
+        res.send("Missing Fields");
+        return;
+    }
+    
+    const user=await prisma.user.findUnique({
+        where:{
+            email
+        }
+    })
+
+    if(!user){
+        res.send("User doesnt exist");
+        return;
+    }
+
+    const validPassword= await bcrypt.compare(password,user.password);
+
+    if(!validPassword){
+        res.send("Incorrect Password");
+        return;
+    }
+
+    const token=jwt.sign({userId:user.id},process.env.JWT_SECRET!,{ expiresIn: "7d" });
+    
+    res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "lax",                          
+            secure: process.env.NODE_ENV === "production", 
+            maxAge: 1000 * 60 * 60 * 24 * 7           
+        });
+
+        res.send("Login successfull");
+})
+
+app.post('/logout',(req:Request,res:Response)=>{
+    res.clearCookie("token");
+    res.send("Logout Successfull")
+})
 
 app.listen(7070, () => {
     console.log("started at http://localhost:7070");
