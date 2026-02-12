4c4448af4a61cc692834fbb21268da4b3beb67b2
AryanOne <xoneder1010@gmail.com>
2026-02-12 05:11:15 +0000

    input taker

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,29 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String args[]) {
-    File myFile = new File("Green.txt");
-    try {
-      if (myFile.createNewFile()) {
-        System.out.println("Created a File name " + myFile.getName());
-      } else {
-        System.out.println("File already exist or not created");
+    Scanner input = new Scanner(System.in);
+
+    String command = input.nextLine();
+
+    if (command.equals("revix init")) {
+      File myFile = new File("Green.txt");
+      try {
+        if (myFile.createNewFile()) {
+          System.out.println("Created a File name " + myFile.getName());
+        } else {
+          System.out.println("File already exist or not created");
+        }
+      } catch (IOException e) {
+        System.out.println("Error Occured");
+        e.printStackTrace();
       }
-    } catch (IOException e) {
-      System.out.println("Error Occured");
-      e.printStackTrace();
+    } else {
+      System.out.println("command dont exist");
     }
+
+    input.close();
   }
 }
